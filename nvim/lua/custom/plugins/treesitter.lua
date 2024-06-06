d0a41a3585da959de9ffbdd516155d09fc0b5b41
David Matos <davidmatos06@gmail.com>
2024-06-06 14:29:14 +0200

    add treesitter related stuff

--- nvim/lua/custom/plugins/treesitter.lua
+++ nvim/lua/custom/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = function()
-        require("nvim-treesitter.install").update({ with_sync = true })()
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  "nvim-treesitter/nvim-treesitter-textobjects",
+  build = function()
+    require("nvim-treesitter.install").update { with_sync = true }()
+  end,
 }
-
