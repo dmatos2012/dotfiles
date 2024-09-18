329593f2a5582e94eb3b311ff786756cb4f9722c
David Matos <davidmatos06@gmail.com>
2024-09-18 11:43:44 +0200

    new stuff

---
+++ nvim/after/plugin/supermaven.lua
@@ -0,0 +1,17 @@
+require("supermaven-nvim").setup {
+  keymaps = {
+    accept_suggestion = "<M-n>",
+    clear_suggestion = "<M-]>",
+    accept_word = "<M-j>",
+  },
+  ignore_filetypes = { cpp = true },
+  -- color = {
+  --   suggestion_color = "#ffffff",
+  --   cterm = 244,
+  -- },
+  disable_inline_completion = false, -- disables inline completion for use with cmp
+  disable_keymaps = false, -- disables built in keymaps for more manual control
+  -- condition = function()
+  --   return false
+  -- end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+}
---
+++ nvim/lua/custom/plugins/supermaven.lua
@@ -0,0 +1,11 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  config = function()
+    require("supermaven-nvim").setup {
+      log_level = "off", -- set to off to disable logging completely
+      condition = function()
+        return false
+      end,
+    }
+  end,
+}
