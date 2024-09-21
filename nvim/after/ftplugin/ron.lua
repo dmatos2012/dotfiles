a8633965416e59bf176b7a02761f75f887d372a1
David Matos <davidmatos06@gmail.com>
2024-09-21 11:17:05 +0200

    this seems like it works????

--- nvim/after/ftplugin/ron.lua
+++ nvim/after/ftplugin/ron.lua
@@ -10,6 +10,34 @@
 --   callback = FormatRonFile,
 -- })
 --
+local format_graphql_str = function(node_text)
+  local node_text_clean = string.sub(node_text, 4, string.len(node_text) - 2)
+  -- print(vim.inspect(node_text_clean))
+  local cmd = vim.system(
+    { "prettier", "--parser", "graphql", "--print-width", "150", "--tab-width", "4" },
+    { stdin = node_text_clean, text = true }
+  )
+  -- print(vim.inspect(node_text_clean))
+  local res = cmd:wait()
+  local formatted_raw = res.stdout
+  print(vim.inspect(formatted_raw))
+  --
+  -- convert the formatted string to array of lines
+  if formatted_raw ~= nil then
+    formatted_raw = formatted_raw:gsub("\n$", "")
+  end
+  print(vim.inspect(formatted_raw))
+  print "___________________________"
+  local formatted = vim.split(formatted_raw, "\n")
+  -- Add indentation to match the rest
+  -- Since this is *only* for `query`, we can hardcode the indentation
+  -- to match what my `formatter` should output which is 4 spaces
+  local indent = string.rep(" ", 4)
+  for idx, line in ipairs(formatted) do
+    formatted[idx] = indent .. line
+  end
+  return formatted
+end
 
 local embedded_graphql = vim.treesitter.query.parse(
   "ron",
@@ -17,9 +45,10 @@ local embedded_graphql = vim.treesitter.query.parse(
 ((struct_entry
   (identifier) @query (#eq? @query "query")
   (string
-    (raw_string) @graphql
-    (#offset! @graphql 1 0 0 -3))))
-    ;;(#offset! @graphql 1 0 0 0))))
+    (raw_string) @graphql)))
+    ;; apparently offset isnt useful in using the captures
+    ;; and it will alwayas return the same `node:range()`
+    ;; so we can remove it?
 ]]
 )
 
@@ -33,7 +62,7 @@ end
 local fmt_graphql = function(bufnr)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   local root = get_root(bufnr)
-  local changes = {}
+  local change = {}
   for id, node in embedded_graphql:iter_captures(root, bufnr, 0, -1) do
     local name = embedded_graphql.captures[id]
     -- we only care about the capture named "ruststring"
@@ -41,80 +70,24 @@ local fmt_graphql = function(bufnr)
       -- {start_row, start_col, end_row, end_col}
       local range = { node:range() } -- span of the capture
       -- {8,11,73,7}
-      print(vim.inspect(range))
       local node_text = vim.treesitter.get_node_text(node, bufnr)
-      -- print(vim.inspect(node_text))
+      local formatted = format_graphql_str(node_text)
+      -- Given that I replace the last line entirely, I need to add back the raw
+      -- string end at the end of the last line,
+      formatted[#formatted] = formatted[#formatted] .. [["#,]]
+      -- range is now {8,11,16,7} in editor lines {9,12,17,8}
+      change.start_row = range[1] + 1
+      -- change.start_row = 1
+      change.start_col = 1
+      -- end row is inclusive so where you end it, it will remove
+      --the contents of that line with whatever you put in there
+      change.end_row = range[3] + 1
+      change.end_col = 1
+      change.formatted = formatted
 
-      -- local formatted, err = vim.system({ "rustfmt" }, { stdin = node_text })
-      -- print(vim.inspect(node_text))
-      -- Trim the raw_string, meaning r#""# in the node_text
-      local node_text_clean = string.sub(node_text, 4, string.len(node_text) - 2)
-      local cmd = vim.system(
-        { "prettier", "--parser", "graphql", "--print-width", "150", "--tab-width", "4" },
-        { stdin = node_text_clean, text = true }
-      )
-      -- print(vim.inspect(node_text_clean))
-      local res = cmd:wait()
-      local formatted_raw = res.stdout
-      -- print(vim.inspect(formatted_raw))
-      -- convert the formatted string to array of lines
-      local formatted_raw = formatted_raw:gsub("\n$", "")
-      -- print(vim.inspect(formatted_raw))
-      -- TODO: CHECK NILLL FOR \n
-      -- print(vim.inspect(rw))
-      print "___________________________"
-      local formatted = vim.split(formatted_raw, "\n")
-      -- local formatted = { "kahlua", "mayor" }
-      -- print(vim.inspect(formatted))
-      -- print(vim.inspect { "hello", "hello2", "}" })
-      -- Add indentation to match the rest
-      print(vim.inspect(range[2]))
-      -- Since this is *only* for `query`, we can hardcode the indentation
-      -- to match what my `formatter` should output which is 4 spaces
-      -- local indent = string.rep(" ", range[2])
-      local indent = string.rep(" ", 4)
-      for idx, line in ipairs(formatted) do
-        formatted[idx] = indent .. line
-      end
-
-      -- print(vim.inspect(node_text))
-      -- print(vim.inspect(formatted))
-      -- Keep track of the changes
-      -- But insert in reverse order of file,
-      -- so that when we make modifications, we dont have
-      -- any out of date line numbers
-      table.insert(changes, 1, {
-        -- {10,11,57,7} ..add 1 to match your editor
-        -- you can also specify start_row, start_col if you need to be more precise
-        start_row = range[1] + 1, -- start line
-        start_col = range[2] + 1, -- start col
-        final_row = range[3] + 1, -- final line
-        final_col = range[4] + 1, -- final col
-        formatted = formatted,
-        -- formatted = {"kahllua", "mayor"},
-      })
-      for _, change in ipairs(changes) do
-        vim.api.nvim_buf_set_lines(bufnr, change.start_row, change.final_row, false, change.formatted)
-        print(vim.inspect(change.end_row))
-
-        print(vim.inspect(#formatted))
-        -- The "# needs to account for the difference in fmt vs original text
-        local actual_final_row = range[1] + #formatted
-        local last_col = vim.api.nvim_buf_get_lines(bufnr, actual_final_row - 1, actual_final_row, false)[1]
-        -- local new_last_line = last_col .. [["#,]]
-        -- vim.api.nvim_buf_set_lines(bufnr, actual_final_row - 1, actual_final_row, false, { new_last_line })
-        -- -- vim.api.nvim_buf_set_text(
-        --
-        --   bufnr,
-        --   -- change.final_row - 1,
-        --   actual_final_row - 1,
-        --   #last_col, --1 WORKS
-        --   actual_final_row - 1,
-        --   -- change.final_row - 1,
-        --   #last_col, -- 1 WORKS
-        --   { [["#]] }
-        -- )
-      end
+      -- local tempbufnr = 61
+      local tempbufnr = bufnr
+      vim.api.nvim_buf_set_lines(tempbufnr, change.start_row, change.end_row, false, change.formatted)
     end
   end
 end
--- nvim/after/queries/ron/injections.scm
+++ nvim/after/queries/ron/injections.scm
@@ -2,12 +2,14 @@
 ; Somehow this below works in expanding the original syntax tree
 ; But if you remove one set of parenthesis, it doesnt work
 ; meaning ((struct_entry).... vs (struct_entry)....
+;; Check content onto offset why it makes difference or not
+;; BELOW CODE IS WORKING!!
 ;
-((struct_entry
-  (identifier) @name (#eq? @name "query")
-  (string
-    (raw_string) @injection.content)
-    (#offset! @injection.content 1 -7 0 -2)
-    (#set! injection.language "graphql")))
+;((struct_entry
+;  (identifier) @name (#eq? @name "query")
+;  (string
+;    (raw_string) @injection.content)
+;    (#offset! @injection.content 1 -7 0 -2)
+;    (#set! injection.language "graphql")))
 ;;;
 
