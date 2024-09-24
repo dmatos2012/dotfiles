d821433a3170b209b32cf32549e4582736509a68
David Matos <davidmatos06@gmail.com>
2024-09-24 15:46:54 +0200

    seems slow but this approach works?

--- nvim/after/ftplugin/ron.lua
+++ nvim/after/ftplugin/ron.lua
@@ -10,6 +10,17 @@
 --   callback = FormatRonFile,
 -- })
 --
+local format_ron_str = function(buf_content)
+  local cmd = vim.system(
+    { "/home/david/ron-formatter/target/debug/ron-formatter" },
+    { stdin = table.concat(buf_content, "\n"), text = true }
+  )
+  local res = cmd:wait()
+  -- print(vim.inspect(res))
+  local ron_str = res.stdout
+  print(vim.inspect(ron_str))
+  return ron_str
+end
 local format_graphql_str = function(node_text)
   local node_text_clean = string.sub(node_text, 4, string.len(node_text) - 2)
   -- print(vim.inspect(node_text_clean))
@@ -20,14 +31,14 @@ local format_graphql_str = function(node_text)
   -- print(vim.inspect(node_text_clean))
   local res = cmd:wait()
   local formatted_raw = res.stdout
-  print(vim.inspect(formatted_raw))
+  -- print(vim.inspect(formatted_raw))
   --
   -- convert the formatted string to array of lines
   if formatted_raw ~= nil then
     formatted_raw = formatted_raw:gsub("\n$", "")
   end
-  print(vim.inspect(formatted_raw))
-  print "___________________________"
+  -- print(vim.inspect(formatted_raw))
+  -- print "___________________________"
   local formatted = vim.split(formatted_raw, "\n")
   -- Add indentation to match the rest
   -- Since this is *only* for `query`, we can hardcode the indentation
@@ -60,6 +71,10 @@ local get_root = function(bufnr)
 end
 
 local fmt_graphql = function(bufnr)
+  local buf_content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local formatted_ron = format_ron_str(buf_content)
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(formatted_ron, "\n"))
+  -- print(vim.inspect(formatted_ron))
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   local root = get_root(bufnr)
   local change = {}
@@ -71,6 +86,7 @@ local fmt_graphql = function(bufnr)
       local range = { node:range() } -- span of the capture
       -- {8,11,73,7}
       local node_text = vim.treesitter.get_node_text(node, bufnr)
+      -- local preformatted = format_ron_str(j
       local formatted = format_graphql_str(node_text)
       -- Given that I replace the last line entirely, I need to add back the raw
       -- string end at the end of the last line,
@@ -78,15 +94,15 @@ local fmt_graphql = function(bufnr)
       -- range is now {8,11,16,7} in editor lines {9,12,17,8}
       change.start_row = range[1] + 1
       -- change.start_row = 1
-      change.start_col = 1
       -- end row is inclusive so where you end it, it will remove
       --the contents of that line with whatever you put in there
       change.end_row = range[3] + 1
-      change.end_col = 1
       change.formatted = formatted
 
       -- local tempbufnr = 61
       local tempbufnr = bufnr
+      -- repalce the whole buffer with hello world
+      -- vim.api.nvim_buf_set_lines(tempbufnr, 0, -1, false, { "kahlua" })
       vim.api.nvim_buf_set_lines(tempbufnr, change.start_row, change.end_row, false, change.formatted)
     end
   end
