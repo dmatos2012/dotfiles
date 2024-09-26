1c34dd7594b203a16a592fae4af1a48abdd5481a
David Matos <davidmatos06@gmail.com>
2024-09-26 14:01:27 +0200

    add highlighting again

--- nvim/after/queries/ron/injections.scm
+++ nvim/after/queries/ron/injections.scm
@@ -5,11 +5,16 @@
 ;; Check content onto offset why it makes difference or not
 ;; BELOW CODE IS WORKING!!
 ;
-;((struct_entry
-;  (identifier) @name (#eq? @name "query")
-;  (string
-;    (raw_string) @injection.content)
-;    (#offset! @injection.content 1 -7 0 -2)
-;    (#set! injection.language "graphql")))
+((struct_entry
+  (identifier) @name (#eq? @name "query")
+  (string
+    (raw_string) @injection.content)
+    ;;remove the opening r#" and any whitespace up to the first non-empty line.
+    (#gsub! @injection.content "^r#\"\\s*\n\\s*" "") ;;
+    ;;remove the closing "# and any preceding whitespace.
+    (#gsub! @injection.content "\\s*\"#$" "")
+    ;; Removed offset since it wasnt very robust
+    ;(#offset! @injection.content 1 -7 0 0)
+    (#set! injection.language "graphql")))
 ;;;
 
