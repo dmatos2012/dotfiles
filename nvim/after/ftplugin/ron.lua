4c5189c62d02af8c222e42f18e534cefd3ae4aac
David Matos <davidmatos06@gmail.com>
2024-09-26 13:38:38 +0200

    is this correct now?

--- nvim/after/ftplugin/ron.lua
+++ nvim/after/ftplugin/ron.lua
@@ -1,16 +1,4 @@
--- function FormatRonFile()
---   vim.cmd "echo 'Formatting ron file'"
---   print "Formatting ron file"
---   -- execute print to stdout
--- end
--- local group = vim.api.nvim_create_augroup("fmtGroup", { clear = true })
--- vim.api.nvim_create_autocmd({ "BufWritePre" }, {
---   pattern = "*.ron",
---   group = group,
---   callback = FormatRonFile,
--- })
---
-local format_ron_str = function(buf_content)
+local format_ron_content = function(buf_content)
   local cmd = vim.system(
     { "/home/david/ron-formatter/target/debug/ron-formatter" },
     { stdin = table.concat(buf_content, "\n"), text = true }
@@ -27,13 +15,15 @@ local format_graphql_str = function(node_text)
   )
   local res = cmd:wait()
   local formatted_raw = res.stdout
+  local formatted = {}
   -- convert the formatted string to array of lines
   if formatted_raw ~= nil then
     formatted_raw = formatted_raw:gsub("\n$", "")
+    formatted = vim.split(formatted_raw, "\n")
   end
   -- print(vim.inspect(formatted_raw))
   -- print "___________________________"
-  local formatted = vim.split(formatted_raw, "\n")
+  -- local formatted = vim.split(formatted_raw, "\n")
   -- Add indentation to match the rest
   -- Since this is *only* for `query`, we can hardcode the indentation
   -- to match what my `formatter` should output which is 4 spaces
@@ -69,18 +59,18 @@ end
 -- And then rescanning it to format the `query` string,
 -- but ok this is just a hobby and making the idea work :)
 -- However, the `prettier` part is the BIGGEST bottleneck
-local fmt_graphql = function(bufnr)
+local fmt = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
   -- rewrite the buffer with the `ron-formatter`
   local buf_content = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
-  local formatted_ron = format_ron_str(buf_content)
+  local formatted_ron = format_ron_content(buf_content)
   vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, vim.split(formatted_ron, "\n"))
   -- rewrite the buffer with the `ron-formatter`
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
   local root = get_root(bufnr)
   local change = {}
   for id, node in embedded_graphql:iter_captures(root, bufnr, 0, -1) do
     local name = embedded_graphql.captures[id]
-    -- we only care about the capture named "ruststring"
+    -- we only care about the capture named "graphql"
     if name == "graphql" then
       -- {start_row, start_col, end_row, end_col}
       local range = { node:range() } -- span of the capture
@@ -101,4 +91,13 @@ local fmt_graphql = function(bufnr)
   end
 end
 
-fmt_graphql(4)
+local group = vim.api.nvim_create_augroup("fmtGroup", { clear = true })
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = "*.ron",
+  group = group,
+  callback = function(args)
+    local bufnr = args.buf
+    fmt(bufnr)
+  end,
+})
+--
