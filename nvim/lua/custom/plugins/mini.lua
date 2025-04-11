3358fb5fdf1ed0df8a2be71d4ed9f428ba320e71
David Matos <davidmatos06@gmail.com>
2025-04-11 09:23:59 +0200

    add minified plugins

--- nvim/lua/custom/plugins/mini.lua
+++ nvim/lua/custom/plugins/mini.lua
@@ -3,7 +3,10 @@ return {
     "echasnovski/mini.nvim",
     config = function()
       require("mini.ai").setup()
+      require("mini.hipatterns").setup()
       require("mini.surround").setup()
+      require("mini.move").setup()
+      require("mini.bufremove").setup()
     end,
   },
 }
